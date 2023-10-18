d8cadfea673c270a0d60efef8a1f4daa807b8d97
Jenna Van De Vyver <r0889235@student.thomasmore.be>
2023-10-18 23:25:57 +0200

    App.js - created scrollview + tilesContainer + Course

--- App.js
+++ App.js
@@ -1,20 +1,44 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image, ScrollView } from 'react-native';
+
+import Course from './components/Course';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <ScrollView style={styles.container}> 
+    {/* scrollview-container that allows you to scroll */}
+      <Text style={styles.header1}>Courses</Text>
+
+      <View style={styles.tilesContainer}>
+        <Course title="Development 1" subtitle="YPO365" />
+        
+        <StatusBar style="auto" />
+      </View>  
+
+
+    {/* tile nr.1 */}
+
+    </ScrollView>  
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  header1: {
+    fontSize: 32,
+    fontWeight: '800',
+    margin: 12,
   },
+  container: {
+    backgroundColor: '#fff',
+    paddingTop: 56,
+  },
+  tilesContainer: {
+    flex: 1,
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+    flexGrow: 0,
+    flexShrink: 0,
+    justifyContent: 'space-evenly',
+
+  }
 });
