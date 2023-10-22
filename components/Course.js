49e4f85db24385f289354f651c3e040956d53ef4
Jenna Van De Vyver <r0889235@student.thomasmore.be>
2023-10-22 15:30:33 +0200

    Course.js - added component const Course props - added style

--- components/Course.js
+++ components/Course.js
@@ -1,1 +1,46 @@
 import { StyleSheet, Text, View, Image } from 'react-native';
+
+const Course = props => {
+    /* Component Course - properties - */
+    return  (
+        /* Build up your tile structure here */
+        <View style={styles.tile}>
+            <Image style={styles.tileImage} source={require('./../assets/courseImage/macbook-code.jpg')}/>
+
+            <View style={styles.tileDetail}>
+                <Text styele={styles.tileHeader}>{props.title}</Text>
+                <Text styele={styles.tileText}>{props.subtitle}</Text>
+            </View>    
+        </View>
+    )
+}
+
+export default Course;
+
+const styles = StyleSheet.create({
+    tile: {
+        flexBasis: '46%',//200
+        borderRadius: 5,
+        borderColor: '#B4B8DA',
+        borderWidth: 1,
+        borderStyle: 'solid',
+        marginVertical: '2%',
+    
+      },
+      tileImage: {
+        width: '100%',
+        maxHeight: 110,
+        overflow: 'hidden',
+      },
+      tileDetail: {
+        padding: 10,
+      },
+      tileHeader: {
+        fontSize: 22,
+        fontWeight: '600',
+      },
+      tileText: {
+        fontSize: 16,
+        fontWeight: '500',
+      }
+    });
